b40bbdd2c793f843632da866f3dac26e43494a43
alxest <youngju.song@sf.snu.ac.kr>
2015-08-28 03:22:48 +0900

    Commit initially.

---
+++ Compiler/Program.fs
@@ -0,0 +1,41 @@
+﻿module mainmodule
+
+open Microsoft.CodeAnalysis;
+open Microsoft.CodeAnalysis.CSharp;
+open Microsoft.CodeAnalysis.CSharp.Syntax;
+open System;
+open System.IO;
+open System.Text;
+
+let rec matching (arg: SyntaxNode, tab: int) = 
+    
+    for i = 1 to tab do
+        Console.Write("    ");
+
+    match arg.Kind() with
+        | _ -> Console.WriteLine(arg.GetType().ToString().Substring(37))
+
+    for node in arg.ChildNodes() do
+        matching(node, tab + 1)
+
+[<EntryPoint>]
+let main argv = 
+    let sampleCode = @"using System;
+    class Program
+    {
+        
+        static void Main()
+        {
+            Console.WriteLine(""Hello, World!"");
+        }
+    }"
+
+    let tree = CSharpSyntaxTree.ParseText(sampleCode)
+    let compilation = CSharpCompilation.Create("SampleCode").AddSyntaxTrees(tree);
+    let model = compilation.GetSemanticModel(tree, true);
+    let root = tree.GetRoot() :?> CompilationUnitSyntax
+    Console.WriteLine("Syntax Tree");
+    matching(root, 0)
+    let foo = Console.ReadLine(); 
+
+    0 
